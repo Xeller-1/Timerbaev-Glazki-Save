df82333a3b751303c54c7db3d703482e7be64692
JeyBarrette <olegpetrow2001@gmail.com>
2023-11-09 16:57:03 +0500

    Добавьте файлы проекта.

---
+++ Timerbaev-Глазки-Save/AgentPage.xaml.cs
@@ -0,0 +1,219 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Timerbaev_Глазки_Save
+{
+    /// <summary>
+    /// Логика взаимодействия для AgentPage.xaml
+    /// </summary>
+    public partial class AgentPage : Page
+        
+    {
+
+        int CountRecords;
+        int CountPage;
+        int CurrentPage = 0;
+        List<Agent> CurrentPageList = new List<Agent>();
+        List<Agent> TableList;
+
+        public AgentPage()
+        {
+            InitializeComponent();
+
+            var currentAgent = Timerbaev_agentEntities.GetContext().Agent.ToList();
+
+            AgentListView.ItemsSource = currentAgent;
+
+            ComboType.SelectedIndex = 0;
+            ComboType2.SelectedIndex = 0;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Manager.MainFrame.Navigate(new AddEditPage());
+        }
+        private void TBoxSearch_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            UpdateAgent();
+        }
+
+        private void ComboType_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            UpdateAgent();
+        }
+
+        private void ComboType2_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            UpdateAgent();
+        }
+        private void UpdateAgent()
+        {
+            var currentAgent = Timerbaev_agentEntities.GetContext().Agent.ToList();
+
+
+            if (ComboType.SelectedIndex == 1)
+            {
+                currentAgent = currentAgent.OrderByDescending(p => p.Priority).ToList();
+            }
+            if (ComboType.SelectedIndex == 2)
+            {
+                currentAgent = currentAgent.OrderBy(p => p.Priority).ToList();
+            }
+            if (ComboType.SelectedIndex == 3)
+            {
+                currentAgent = currentAgent.OrderByDescending(p => p.Title).ToList();
+            }
+            if (ComboType.SelectedIndex == 4)
+            {
+                currentAgent = currentAgent.OrderBy(p => p.Title).ToList();
+            }
+
+            if (ComboType2.SelectedIndex == 0)
+            {
+                currentAgent = currentAgent;
+            }
+            if (ComboType2.SelectedIndex == 1)
+            {
+                currentAgent = currentAgent.Where(p => p.AgentTypeString == "МФО").ToList();
+            }
+            if (ComboType2.SelectedIndex == 2)
+            {
+                currentAgent = currentAgent.Where(p => p.AgentTypeString == "ЗАО").ToList();
+            }
+            if (ComboType2.SelectedIndex == 3)
+            {
+                currentAgent = currentAgent.Where(p => p.AgentTypeString == "МКК").ToList();
+            }
+            if (ComboType2.SelectedIndex == 4)
+            {
+                currentAgent = currentAgent.Where(p => p.AgentTypeString == "ОАО").ToList();
+            }
+            if (ComboType2.SelectedIndex == 5)
+            {
+                currentAgent = currentAgent.Where(p => p.AgentTypeString == "ПАО").ToList();
+            }
+            if (ComboType2.SelectedIndex == 6)
+            {
+                currentAgent = currentAgent.Where(p => p.AgentTypeString == "ООО").ToList();
+            }
+
+            currentAgent = currentAgent.Where(p => PhoneFormat(p.Phone.ToLower()).Trim().Contains(TBoxSearch.Text.ToLower()) || p.Title.ToLower().Contains(TBoxSearch.Text.ToLower())
+            || p.Email.ToLower().Contains(TBoxSearch.Text.ToLower())).ToList();
+            AgentListView.ItemsSource = currentAgent;
+
+            TableList = currentAgent;
+            ChangePage(0, 0);
+
+        }
+        private string PhoneFormat(string phone)
+        {
+            return phone.Replace("(", "").Replace(")", "").Replace("-", "").Replace(" ", "");
+        }
+
+        private void ChangePage(int direction, int? selectedPage)
+        {
+            CurrentPageList.Clear();
+            CountRecords = TableList.Count;
+            if (CountRecords % 10 > 0)
+            {
+                CountPage = CountRecords / 10 + 1;
+            }
+            else
+            {
+                CountPage = CountRecords / 10;
+            }
+            var ifUpdate = true;
+            int min;
+            if (selectedPage.HasValue)
+            {
+                if (selectedPage >= 0 && selectedPage <= CountPage)
+                {
+                    CurrentPage = (int)selectedPage;
+                    min = CurrentPage * 10 + 10 < CountRecords ? CurrentPage * 10 + 10 : CountRecords;
+                    for (int i = CurrentPage * 10; i < min; i++)
+                    {
+                        CurrentPageList.Add(TableList[i]);
+                    }
+                }
+            }
+            else
+            {
+                switch (direction)
+                {
+                    case 1:
+                        if (CurrentPage > 0)
+                        {
+                            CurrentPage--;
+                            min = CurrentPage * 10 + 10 < CountRecords ? CurrentPage * 10 + 10 : CountRecords;
+                            for (int i = CurrentPage * 10; i < min; i++)
+                            {
+                                CurrentPageList.Add(TableList[i]);
+                            }
+                        }
+                        else
+                        {
+                            ifUpdate = false;
+                        }
+                        break;
+                    case 2:
+                        if (CurrentPage < CountPage - 1)
+                        {
+                            CurrentPage++;
+                            min = CurrentPage * 10 + 10 < CountRecords ? CurrentPage * 10 + 10 : CountRecords;
+                            for (int i = CurrentPage * 10; i < min; i++)
+                            {
+                                CurrentPageList.Add(TableList[i]);
+                            }
+                        }
+                        else
+                        {
+                            ifUpdate = false;
+                        }
+                        break;
+                }
+            }
+            if (ifUpdate)
+            {
+                PageListBox.Items.Clear();
+                for (int i = 1; i <= CountPage; i++)
+                {
+                    PageListBox.Items.Add(i);
+                }
+                PageListBox.SelectedIndex = CurrentPage;
+
+                min = CurrentPage * 10 + 10 < CountRecords ? CurrentPage * 10 + 10 : CountRecords;
+                TBCount.Text = min.ToString();
+                TBAllRecords.Text = " из " + CountRecords.ToString();
+
+                AgentListView.ItemsSource = CurrentPageList;
+                AgentListView.Items.Refresh();
+            }
+        }
+
+        private void PageListBox_MouseUp(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            ChangePage(0, Convert.ToInt32(PageListBox.SelectedItem.ToString()) - 1);
+        }
+        private void LeftDirButton_Click(object sender, RoutedEventArgs e)
+        {
+            ChangePage(1, null);
+        }
+        private void RightDirButton_Click(object sender, RoutedEventArgs e)
+        {
+            ChangePage(2, null);
+        }
+
+    }
+}
